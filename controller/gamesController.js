73d6b84d3f6401821d0e90e9c07a772793ae3324
Kono <anupam.nitrkl@gmail.com>
2025-07-02 18:06:41 +0530

    fix: fixed sql syntax in query

--- controller/gamesController.js
+++ controller/gamesController.js
@@ -1,6 +1,6 @@
 const db =  require('../db/query.js');
 
 async function getGame(req,res){
-    const game = await db.getGame(id); 
+    const game = await db.getGame(req.params.id); 
     res.send('Your Game is: '+game);
 }
--- db/query.js
+++ db/query.js
@@ -1,10 +1,15 @@
 const pool = require('./pool');
 
 async function getAllGames(){
-    const {rows} = await pool.query('SELECT * FROM GAMES')
+    const {rows} = await pool.query('SELECT * FROM games')
     return rows;
 }
 
+async function getGame(id){
+    const { rows } = await pool.query('SELECT title FROM games WHERE id = $1', [id]);
+    return row;
+}
 module.exports = {
-    getAllGames
+    getAllGames,
+    getGame 
 };
